6c89d00a13ab0a285b4e83f41225cf97f950cd2b
NLMitterecker <d.mitterecker@student.vu.nl>
2020-10-04 22:07:03 +0200

    DEV: finished chapter 1

--- django3_by_example/ch1/myfirst_project/blog/urls.py
+++ django3_by_example/ch1/myfirst_project/blog/urls.py
@@ -5,4 +5,7 @@ app_name = 'app'
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+        views.post_details,
+        name='post_details')
 ]
